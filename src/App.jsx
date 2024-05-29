df417f7f2586f9e544a429bbf023e3876aa34860
Prajakta Dhage <99481665+prajaktagd@users.noreply.github.com>
2024-05-29 16:35:27 +0530

    Add logic for loading state

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,24 @@ import Header from "./components/Header";
 import PostsList from "./components/PostsList";
 import Modal from "./components/Modal";
 import NewPost from "./components/NewPost";
+import styles from "./App.module.css";
 
 function App() {
   const [isPosting, setIsPosting] = useState(false);
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const stopPostingHandler = () => setIsPosting(false);
   const startPostingHandler = () => setIsPosting(true);
 
   useEffect(() => {
     const fetchPosts = async () => {
+      setIsLoading(true);
       const existingPosts = await fetch("http://localhost:8080/posts")
         .then((res) => res.json())
         .then((json) => json.posts);
       setPosts(existingPosts);
+      setIsLoading(false);
     };
     fetchPosts();
   }, []);
@@ -31,6 +35,12 @@ function App() {
     setPosts((existingPosts) => [newPost, ...existingPosts]);
   };
 
+  const postsHtml = <PostsList posts={posts} />;
+  const noPostsHtml = (
+    <div className={styles.noPosts}>There are no posts yet!</div>
+  );
+  const loadingHtml = <div className={styles.loading}>Loading posts...</div>;
+
   return (
     <>
       <Header onCreatePost={startPostingHandler} />
@@ -40,7 +50,7 @@ function App() {
             <NewPost onClose={stopPostingHandler} onAddPost={addPostHandler} />
           </Modal>
         )}
-        <PostsList posts={posts} />
+        {isLoading ? loadingHtml : posts.length ? postsHtml : noPostsHtml}
       </main>
     </>
   );
